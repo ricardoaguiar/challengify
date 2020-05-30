cdce7c1d5184c36fe58c301d231063b833a2721d
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2020-05-30 23:41:58 +0200

    Made progress with create challenge view

--- src/views/createChallenge.js
+++ src/views/createChallenge.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 import View from '../components/viewSkeleton/viewSkeleton'
 import InputField from '../components/inputField/inputField'
+import Gap from '../components/gap/gap'
+import RadioButtonGroup from '../components/radioButtonGroup/radioButtonGroup'
 
 const CreateChallenge = () => (
   <View
@@ -17,6 +19,14 @@ const CreateChallenge = () => (
       id='newChallengeTitle'
       label='Title'
       placeholder='e.g. Smoke less'
+    />
+    <Gap size='big' />
+    <RadioButtonGroup
+      options={[
+        {label: 'Target', selected: true},
+        {label: 'Limit'},
+        {label: 'Track'}
+      ]}
     />
   </View>
 )
