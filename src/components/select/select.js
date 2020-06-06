b1b9cc1d9ba5c67459bfcb3dd5f592d969b52ad7
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2020-06-07 00:00:36 +0200

    Created select component

---
+++ src/components/select/select.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import './select.css'
+
+const Select = ({
+  id,
+  label,
+  children,
+  ...props
+}) => (
+  <div className='select'>
+    <label htmlFor={id}>
+      {label}
+    </label>
+    <select {...{id, ...props}}>
+      {children}
+    </select>
+  </div>
+)
+export default Select
