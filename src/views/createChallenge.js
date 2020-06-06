4a6d6ca1516a5b8d26a66b20544c6616ebf05863
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2020-06-06 22:13:04 +0200

    Added state for title and date inputs

--- src/views/createChallenge.js
+++ src/views/createChallenge.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import View from '../components/view/view'
 import InputField from '../components/inputField/inputField'
@@ -13,24 +13,65 @@ const getTodaysDate = () => {
   const yyyy = today.getFullYear()
   return `${yyyy}-${mm}-${dd}`
 }
-    />
-    <Gap size='big' />
-    <Columns gapSize='big'>
-      <div>
-        <InputField
-          id='newChallengeStartDate'
-          label='Start date'
-          type='date'
-        />
-      </div>
-      <div>
-        <InputField
-          id='newChallengeEndDate'
-          label='End date'
-          type='date'
-        />
-      </div>
-    </Columns>
-  </View>
-)
+
+const CreateChallenge = () => {
+  const [title, onSetTitle] = useState('')
+  const [startDate, onSetStartDate] = useState(getTodaysDate())
+  const [endDate, onSetEndDate] = useState('')
+  return (
+    <View
+      title='Create new challenge'
+      actions={{
+        left: {
+          to: '..',
+          text: 'Back'
+        }
+      }}
+    >
+      <RadioButtonGroup
+        options={[
+          {label: 'Target', selected: true},
+          {label: 'Limit'},
+          {label: 'Track'}
+        ]}
+      />
+      <Gap size='big' />
+      <InputField
+        id='newChallengeTitle'
+        label='Title'
+        placeholder='e.g. Smoke less'
+        value={title}
+        onChange={({target: {value}}) => {
+          onSetTitle(value)
+        }}
+      />
+      <Gap size='big' />
+      <Columns gapSize='big'>
+        <div>
+          <InputField
+            id='newChallengeStartDate'
+            label='Start date'
+            type='date'
+            value={startDate}
+            onChange={({target: {value}}) => {
+              onSetStartDate(value)
+            }}
+          />
+        </div>
+        <div>
+          <InputField
+            id='newChallengeEndDate'
+            label='End date'
+            type='date'
+            value={endDate}
+            onChange={({target: {value}}) => {
+              onSetEndDate(value)
+            }}
+          />
+        </div>
+      </Columns>
+    </View>
+  )
+}
+
 export default CreateChallenge
