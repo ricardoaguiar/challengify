6d6503e69622d5fb8b59b5b6a69a762b7455e6b6
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2020-10-23 18:36:15 +0200

    Updated store structure

--- src/app/db.js
+++ src/app/db.js
@@ -3,7 +3,7 @@ import Dexie from 'dexie'
 const initializeDatabase = () => {
   const db = new Dexie('challengify')
   db.version(1).stores({
-    challenges: '++id,startTimestamp,archived',
+    challenges: '++id,startTimestamp,archived,completed',
     records: '++id,challengeId,timestamp,value'
   })
   return db
