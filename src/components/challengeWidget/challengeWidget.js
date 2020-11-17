332cad92152ea686ca42e2d70eb8709cee743e9e
Dejene <dejenedaba269@gmail.com>
2020-11-17 21:39:54 +0100

    created component skelet

---
+++ src/components/challengeWidget/challengeWidget.js
@@ -0,0 +1,40 @@
+//import logo from './logo.svg';
+import React from 'react';
+//import fontawesome from '.Components/fontawesome';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <a href="#"><h1>Read more in 2021</h1></a>
+      <div className="cup">
+         <button>+</button>
+         </div>
+      <div className="first_button">
+       <button>Learning</button>
+       <button>Ny resolutions</button>
+       </div>
+
+       
+
+        <div className="paragraph">
+        <p>
+          read at least two books every month <br></br>
+          from 21 January 2021 until 31 December 2021
+        </p>
+        </div>
+
+        <div className="curcles">
+        <button>3</button> 
+        <button>1</button>
+        <button>3</button>
+        <button>2</button>
+        <button>0</button>
+        <button>3</button>
+        <button>+</button>
+        </div>
+        </div>
+  );
+}
+
+export default App;
