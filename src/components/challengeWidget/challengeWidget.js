379c8eb10136b58dbe8cfeead71148f22102c466
Dejene <dejenedaba269@gmail.com>
2020-11-17 22:35:07 +0100

    manner changes

--- src/components/challengeWidget/challengeWidget.js
+++ src/components/challengeWidget/challengeWidget.js
@@ -1,40 +1,38 @@
-//import logo from './logo.svg';
-import React from 'react';
-//import fontawesome from '.Components/fontawesome';
-import './challengeWidget.css';
+import { Link } from "@reach/router";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <a href="#"><h1>Read more in 2021</h1></a>
-      <div className="cup">
-         <button>+</button>
-         </div>
-      <div className="first_button">
-       <button>Learning</button>
-       <button>Ny resolutions</button>
-       </div>
+import "./challengeWidget.css";
 
-       
+const ChallengeWidget = () => (
+  <div className="challengeWidget">
+    <Link to="#">
+      <h1>Read more in 2021</h1>
+    </Link>
+    <div className="cup">
+      <button>+</button>
+    </div>
+    <div className="first_button">
+      <button>Learning</button>
+      <button>Ny resolutions</button>
+    </div>
 
-        <div className="paragraph">
-        <p>
-          read at least two books every month <br></br>
-          from 21 January 2021 until 31 December 2021
-        </p>
-        </div>
+    <div className="paragraph">
+      <p>
+        read at least two books every month <br></br>
+        from 21 January 2021 until 31 December 2021
+      </p>
+    </div>
 
-        <div className="curcles">
-        <button>3</button> 
-        <button>1</button>
-        <button>3</button>
-        <button>2</button>
-        <button>0</button>
-        <button>3</button>
-        <button>+</button>
-        </div>
-        </div>
-  );
-}
+    <div className="curcles">
+      <button>3</button>
+      <button>1</button>
+      <button>3</button>
+      <button>2</button>
+      <button>0</button>
+      <button>3</button>
+      <button>+</button>
+    </div>
+  </div>
+);
 
-export default App;
+export default ChallengeWidget;
