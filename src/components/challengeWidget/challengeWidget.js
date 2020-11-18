de262d6770dbc512cb285fe493af2eddfbf7fcd1
Dejene <dejenedaba269@gmail.com>
2020-11-18 10:12:44 +0100

    Minor changes

--- src/components/challengeWidget/challengeWidget.js
+++ src/components/challengeWidget/challengeWidget.js
@@ -16,14 +16,14 @@ const ChallengeWidget = ({ title, linkTo = "#" }) => (
       <button>Ny resolutions</button>
     </div>
 
-    <div className="paragraph">
+    <div className="discription">
       <p>
         read at least two books every month <br></br>
         from 21 January 2021 until 31 December 2021
       </p>
     </div>
 
-    <div className="curcles">
+    <div className="circules">
       <button>3</button>
       <button>1</button>
       <button>3</button>
